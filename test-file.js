bcbb166dd697d87a2de1415d44448209a846ef59
Jean Paul Santander <jpsantanderios@hotmail.com>
2026-05-11 00:35:23 -0400

    Update test-file.js

--- test-file.js
+++ test-file.js
@@ -1,1 +1,7 @@
-console.log("Hola mundo")
+let variableArray = ["Supermercado", 2, true, "Elemento4", ["lista","cuaderno","Hoja de block"]];
+console.log(variableArray)
+
+// () Para definir la instruccion del algoritmo
+// [] Para definir una lista
+// {} Para definir la accion de un algorito (Ejecucion de comandos para algoritmo condicional if,
+    // o tambien para identificar una variable en un texto ${variable})
