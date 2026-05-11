e9c2487800fc20f2ed954ca7c1460a87eda6851d
Jean Paul Santander <jpsantanderios@hotmail.com>
2026-05-11 01:37:24 -0400

    Update files

--- proyect-x.js
+++ proyect-x.js
@@ -3,3 +3,20 @@
 
 console.log("Hola mundo");
 console.log("Hello world");
+
+let variableString = "Soy un texto";
+let variableNumber = 32;
+let variableBoolean = true; // false
+let variableNull = null;
+let variableUndefined;
+console.log(variableUndefined);
+console.log(variableNull);
+
+console.log(variableObjeto);
+console.log(typeof(variableArray));
+console.log(typeof(variableObjeto));
+console.log(typeof(variableString));
+console.log(typeof(variableNumber));
+console.log(typeof(variableBoolean));
+console.log(typeof(variableNull));
+console.log(typeof(variableUndefined));
--- test-file.js
+++ test-file.js
@@ -1,7 +1,1 @@
-let variableArray = ["Supermercado", 2, true, "Elemento4", ["lista","cuaderno","Hoja de block"]];
-console.log(variableArray)
-
-// () Para definir la instruccion del algoritmo
-// [] Para definir una lista
-// {} Para definir la accion de un algorito (Ejecucion de comandos para algoritmo condicional if,
-    // o tambien para identificar una variable en un texto ${variable})
+console.log("Hola Mundo")
